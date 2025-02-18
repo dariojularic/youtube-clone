ae3926cdac449c55377ff76e0ad355c79cdcbda8
Dario Jularic <dariojularic0@gmail.com>
2025-02-18 10:59:43 +0100

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NewVideos from "./videos/NewVideos";
+import SharedLayout from "./layouts/SharedLayout";
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient} >
-      <NewVideos />
+      <SharedLayout />
     </QueryClientProvider>
   )
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,21 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SharedLayout from "./layouts/SharedLayout.jsx";
+import NewVideos from "./videos/NewVideos.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <SharedLayout />,
+    element: <App />,
     errorElement: <div>404 Not Found</div>,
-    children: [],
+    children: [{ path: "/", element: <NewVideos /> }],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-      {/* <App />
-    </RouterProvider> */}
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>
 );
