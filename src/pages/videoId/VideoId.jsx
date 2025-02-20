4e55446a654c7939a0d028f766074d7c4097d406
Dario Jularic <dariojularic0@gmail.com>
2025-02-20 17:19:33 +0100

    Display youtube video

--- src/pages/videoId/VideoId.jsx
+++ src/pages/videoId/VideoId.jsx
@@ -1,11 +1,11 @@
-import "./VideoId.css"
+import "./VideoId.css";
 import { fetchSingleVideo, options } from "../../api";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Loader from "../../layouts/loader/Loader";
 
 function VideoId() {
-  const params = useParams()
+  const params = useParams();
   // console.log(params)
 
   const { data, error, isLoading } = useQuery({
@@ -13,35 +13,40 @@ function VideoId() {
     queryFn: () => fetchSingleVideo(params.id, options),
   });
 
-  if (error) console.error(error)
+  if (error) console.error(error);
 
-  if (isLoading) return <Loader />
+  if (isLoading) return <Loader />;
 
-  console.log(data.items[0])
+  console.log(data.items[0]);
 
   return (
     <div className="video-id-container">
       <div className="clicked-video-container">
         <div className="display-video-container">
-          <video width="640" height="480" controls >
-            <source src={data.items[0].snippet.thumbnails.standard.url} type="video/mp4" />
+          <video width="640" height="480" controls>
+            <source
+              src={data.items[0].snippet.thumbnails.standard.url}
+              type="video/mp4"
+            />
           </video>
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${params.id}`}
+            // frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </div>
 
-        <div className="video-info">
+        <div className="video-info"></div>
 
-        </div>
-
-        <div className="video-comments">
-
-        </div>
+        <div className="video-comments"></div>
       </div>
 
-      <div className="recommended-videos">
-
-      </div>
+      <div className="recommended-videos"></div>
     </div>
-  )
+  );
 }
 
 export default VideoId;
