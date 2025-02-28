c9dde1f3b33e48fe3735c2076265de35611aa041
Dario Jularic <dariojularic0@gmail.com>
2025-02-28 19:21:18 +0100

    Refactor code

--- src/api/index.js
+++ src/api/index.js
@@ -14,16 +14,16 @@ export const fetchCategoryVideos = async (category, options) => {
   }
 };
 
-export const fetchData = async (url, options) => {
-  try {
-    const response = await fetch(url, options);
-    const data = await response.json();
-    // console.log(data.items)
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
+// export const fetchData = async (url, options) => {
+//   try {
+//     const response = await fetch(url, options);
+//     const data = await response.json();
+//     // console.log(data.items)
+//     return data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
 export const fetchSingleVideo = async (videoId, options) => {
   try {
