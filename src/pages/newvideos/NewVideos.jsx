129b53926c44273dba6b027271c8ca5477521544
Dario Jularic <dariojularic0@gmail.com>
2025-03-10 18:22:12 +0100

    Refactor code

--- src/pages/newvideos/NewVideos.jsx
+++ src/pages/newvideos/NewVideos.jsx
@@ -1,10 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchData, fetchCategoryVideos } from "../../api";
+import { fetchCategoryVideos } from "../../api";
 import VideoCard from "./components/VideoCard";
 import ChannelCard from "./components/ChannelCard";
 import Sidebar from "../../layouts/sidebar/Sidebar";
 import Loader from "../../layouts/loader/Loader";
-import { baseUrl } from "../../api";
 import { options } from "../../api";
 import { useContext } from "react";
 import { categoryContext } from "../../App";
@@ -12,10 +11,7 @@ import { categoryContext } from "../../App";
 import "./NewVideos.css";
 
 function NewVideos() {
-  // const [activeCategory, setActiveCategory] = useState("New");
-  const {activeCategory, setActiveCategory} = useContext(categoryContext)
-  // console.log(activeCategory)
-
+  const { activeCategory, setActiveCategory } = useContext(categoryContext);
 
   const { data, error, isLoading } = useQuery({
     queryKey: ["videos", activeCategory],
@@ -25,30 +21,37 @@ function NewVideos() {
   if (error) console.log("error", error);
 
   if (isLoading) return <Loader />;
-  console.log(data)
-
+  console.log(data);
 
   return (
     <div className="home-grid">
-      <h1><span className="h1-span"> {activeCategory} </span> videos </h1>
-      {/* <div className="home-container"> */}
-        <div className="sidebar-container">
-          <Sidebar activeCategory={activeCategory} setActiveCategory={setActiveCategory}  />
-        </div>
-        <div className="video-grid">
-          {data?.items.map((video) => {
-            if (!video.id.videoId && !video.id.channelId) return
-            return (video.id.kind === "youtube#video") ? <VideoCard key={video.id.videoId} id={video.id.videoId} {...video.snippet} /> : <ChannelCard key={video.id.channelId} id={video.id.channelId} {...video.snippet} />
-            // return (
-            //   <VideoCard
-            //     key={video.id.videoId}
-            //     id={video.id.videoId}
-            //     {...video.snippet}
-            //   />
-            // );
-          })}
-        </div>
-      {/* </div> */}
+      <h1>
+        <span className="h1-span"> {activeCategory} </span> videos{" "}
+      </h1>
+      <div className="sidebar-container">
+        <Sidebar
+          activeCategory={activeCategory}
+          setActiveCategory={setActiveCategory}
+        />
+      </div>
+      <div className="video-grid">
+        {data?.items.map((video) => {
+          if (!video.id.videoId && !video.id.channelId) return;
+          return video.id.kind === "youtube#video" ? (
+            <VideoCard
+              key={video.id.videoId}
+              id={video.id.videoId}
+              {...video.snippet}
+            />
+          ) : (
+            <ChannelCard
+              key={video.id.channelId}
+              id={video.id.channelId}
+              {...video.snippet}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
