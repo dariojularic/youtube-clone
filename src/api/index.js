6a93e52615e309332ef2817b52d39bd5abb9a1a0
Dario Jularic <dariojularic0@gmail.com>
2025-03-13 12:43:57 +0100

    Refactor code

--- src/api/index.js
+++ src/api/index.js
@@ -1,33 +1,30 @@
 // export const baseUrl =
 //   "https://youtube-v31.p.rapidapi.com/search?relatedToVideoId=7ghhRHRP6t4&part=id%2Csnippet&type=video&maxResults=20";
 
-
-
-
 // export const fetchCategoryVideos = async (category, options) => {
 //   try {
 //     const response = await fetch(
-  //       `https://youtube-v31.p.rapidapi.com/search?q=${category}&part=snippet,id&maxResults=24&regionCode=US`,
+//       `https://youtube-v31.p.rapidapi.com/search?q=${category}&part=snippet,id&maxResults=24&regionCode=US`,
 //       options
 //     );
 //     const data = await response.json();
 //     return data;
 //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+//     console.error(error);
+//   }
+// };
 
-  // export const fetchChannelData = async (channelId, options) => {
+// export const fetchChannelData = async (channelId, options) => {
 //   try {
-  //     const response = await fetch(
+//     const response = await fetch(
 //       `https://youtube-v31.p.rapidapi.com/channels?part=snippet,statistics&id=${channelId}`,
 //       options
 //     );
 //     const data = await response.json();
 //     return data;
 //   } catch (error) {
-  //     console.error(error);
-  //   }
+//     console.error(error);
+//   }
 // };
 
 // console.log(import.meta.env.VITE_API_KEY)
@@ -39,8 +36,10 @@
 //   },
 // };
 
-export function fetchFactory() {
-  const baseUrl = "https://youtube-v31.p.rapidapi.com"
+// funkcija buildUrl prima endpoint i query parametre
+
+export function buildUrl(endpoint, value) {
+  const baseUrl = "https://youtube-v31.p.rapidapi.com";
 
   const options = {
     method: "GET",
@@ -50,11 +49,65 @@ export function fetchFactory() {
     },
   };
 
-  const getSingleVideo = (videoId) => `${baseUrl}/videos?part=contentDetails%2Csnippet%2Cstatistics&id=${videoId}`
-  const getVideoComments = (videoId) => `${baseUrl}/commentThreads?part=snippet&videoId=${videoId}&maxResults=8`
-  const getCategoryVideos = (category) => `${baseUrl}/search?q=${category}&part=snippet,id&maxResults=24&regionCode=US`
-  const getChannelData = (channelId) => `${baseUrl}/channels?part=snippet,statistics&id=${channelId}`
-  const getChannelVideos = (channelId) => `${baseUrl}/search?channelId=${channelId}&part=snippet%2Cid&order=date&maxResults=8`
+
+  const queryParams = {
+    part: (endpoint === "videos") ? "contentDetails%2Csnippet%2Cstatistics" : "snippet",
+    videoId: value,
+    "maxResults": 8,
+  };
+
+  const completeUrl = `${baseUrl}/${endpoint}?${new URLSearchParams(queryParams).toString()}`
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(completeUrl, options);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchData()
+
+
+}
+
+// /videos?part=contentDetails%2Csnippet%2Cstatistics&id=3253`
+// commentThreads?part=snippet&videoId=gag3wt2&maxResults=8
+const queryParams = {
+  part: "snippet",
+  videoId: value,
+  "filter[adId]": this.props.id,
+};
+
+// const response = await ccapi({
+//   url: `/ccapi/v4/analytics/ads-visit-records?${new URLSearchParams(queryParams).toString()}`,
+//   method: 'GET'
+// });
+
+export function fetchFactory() {
+  const baseUrl = "https://youtube-v31.p.rapidapi.com";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": import.meta.env.VITE_API_KEY,
+      "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+    },
+  };
+
+  const getSingleVideo = (videoId) =>
+    `${baseUrl}/videos?part=contentDetails%2Csnippet%2Cstatistics&id=${videoId}`;
+  const getVideoComments = (videoId) =>
+    `${baseUrl}/commentThreads?part=snippet&videoId=${videoId}&maxResults=8`;
+  const getCategoryVideos = (category) =>
+    `${baseUrl}/search?q=${category}&part=snippet,id&maxResults=24&regionCode=US`;
+  const getChannelData = (channelId) =>
+    `${baseUrl}/channels?part=snippet,statistics&id=${channelId}`;
+  const getChannelVideos = (channelId) =>
+    `${baseUrl}/search?channelId=${channelId}&part=snippet%2Cid&order=date&maxResults=8`;
 
   const fetchData = async (url) => {
     try {
@@ -66,14 +119,17 @@ export function fetchFactory() {
     }
   };
 
-  return {getCategoryVideos, getChannelData, getChannelVideos, getSingleVideo, getVideoComments, fetchData}
+  return {
+    getCategoryVideos,
+    getChannelData,
+    getChannelVideos,
+    getSingleVideo,
+    getVideoComments,
+    fetchData,
+  };
 }
 
-export const fetchApi = fetchFactory()
-
-
-
-
+export const fetchApi = fetchFactory();
 
 // export const fetchData = async (url, options) => {
 //   try {
